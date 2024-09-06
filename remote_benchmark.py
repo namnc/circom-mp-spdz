e4c0e36bff794456c40e2d60a56e1d453d7b3ee0
Magamedrasul Ibragimov <curryrasul@gmail.com>
2024-09-06 10:08:01 +0300

    chore: update script to run all tests

--- remote_benchmark.py
+++ remote_benchmark.py
@@ -4,7 +4,7 @@ import paramiko
 import subprocess
 import time
 
-EXCLUDED_CIRCUITS = ["circomlib", "circomlib-matrix", "circuits", "crypto", "Keras2Circom"]
+EXCLUDED_CIRCUITS = ["circomlib", "circomlib-matrix", "circuits", "crypto", "Keras2Circom", ".DS_Store"]
 SSH_PORT = 22
 
 def connect_ssh(hostname, port, username="root", password="password"):
@@ -44,36 +44,37 @@ def main():
     else:
         circuits_to_compile = [d for d in os.listdir("ml_tests") if d not in EXCLUDED_CIRCUITS]
 
-    for circuit_name in circuits_to_compile:
-        print(f"Compiling circuit {circuit_name} locally...")
-        subprocess.run(["python3", "main_ml_tests.py", circuit_name])
-        print(f"Compiling {circuit_name} for MP-SPDZ...")
-        os.chdir("../MP-SPDZ")
-        subprocess.run(["./compile.py", "circuit"])
-
     print(f"Connecting to party1 at {party1_ip}:{22} via SSH...")
     ssh = connect_ssh(party1_ip, SSH_PORT)
-
     local_ip = run_remote_command(ssh, "ping -c 1 host.docker.internal | head -1 | awk -F'[()]' '{print $2}'")[0][:-1]
 
+    os.chdir("../MP-SPDZ")
     create_hosts_file()
     run_remote_command(ssh, f'cd ../testing/MP-SPDZ && echo "{local_ip}:3001\n0.0.0.0:{party1_port}" >> hosts')
 
     for circuit_name in circuits_to_compile:
-        print(f"Compiling circuit {circuit_name} remotely on party1...")
+        os.chdir("../circom-mp-spdz")
+        print(f"1. Compiling circuit {circuit_name} to .mpc locally...")
+        subprocess.run(["python3", "main_ml_tests.py", circuit_name], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        print(f"2. Compiling {circuit_name} to bytecode for MP-SPDZ...")
+        os.chdir("../MP-SPDZ")
+        subprocess.run(["./compile.py", "circuit"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+        print(f"3. Compiling circuit {circuit_name} to .mpc remotely on party1...")
         run_remote_command(ssh, f"cd ../testing/circom-mp-spdz && python3 main_ml_tests.py {circuit_name}")
 
-        print(f"Copying {circuit_name}.mpc from local to party1...")
+        print(f"4. Copying {circuit_name}.mpc from local to party1...")
         subprocess.run(["sshpass", "-p", "password", "scp", f"../circom-mp-spdz/outputs/{circuit_name}/circuit.mpc", f"root@{party1_ip}:/testing/MP-SPDZ/Programs/Source/"])
-        output, _ = run_remote_command_verbose(ssh, f"cd ../testing/MP-SPDZ && ./compile.py circuit")
-        print("-----")
-        print(output)
-        print("-----")
+        run_remote_command(ssh, f"cd ../testing/MP-SPDZ && ./compile.py circuit")
 
-    print(f"Running MP-SPDZ with circuit {circuit_name}...")
-    subprocess.Popen(["./semi-party.x", "-N", "2", "-p", "0", "-OF", ".", "circuit", "-ip", "hosts"])
-    time.sleep(1) # to be sure that the process was started
-    run_remote_command(ssh, f"cd ../testing/MP-SPDZ && ./semi-party.x -N 2 -p 1 -OF . circuit -ip hosts")
+        print(f"5. Running MP-SPDZ with circuit {circuit_name}...")
+        spdz_local = subprocess.Popen(["./semi-party.x", "-N", "2", "-p", "0", "-OF", ".", "circuit", "-ip", "hosts"])
+        time.sleep(1) # to be sure that the process was started
+        run_remote_command(ssh, f"cd ../testing/MP-SPDZ && ./semi-party.x -N 2 -p 1 -OF . circuit -ip hosts")
+        spdz_local.wait()
+        print()
+        print("------------------------------")
+        print()
 
     ssh.close()
 
