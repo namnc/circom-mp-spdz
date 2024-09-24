cdb06bfbb80a194d3266b02d3ea929bc458f3d05
Magamedrasul Ibragimov <curryrasul@gmail.com>
2024-09-24 20:36:55 +0300

    chore: accuracy update for Dense & GlobalAveragePooling2D

--- ml_tests/Dense/accuracy.py
+++ ml_tests/Dense/accuracy.py
@@ -1,24 +1,66 @@
 import json
+import subprocess
+import re
+import numpy as np
 from ml_tests.util import DenseInt
+from ml_tests.util import reshape_array
 
 def main():
+    process = subprocess.run(
+        ['python3', 'main_ml_tests.py', 'Dense'],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True
+    )
+
+    stdout = process.stdout
+
+    outputs_match = re.search(r'Outputs:\s*(\{.*?\})', stdout, re.DOTALL)
+    outputs_str = outputs_match.group(1)
+
+    # Convert outputs_str to valid JSON format
+    # Replace single quotes with double quotes
+    outputs_str_json = outputs_str.replace("'", '"')
+    outputs_dict = json.loads(outputs_str_json)
+
+    # Extract indices and values, build a list of (indices, value) pairs
+    indices_value_list = []
+    for key, value in outputs_dict.items():
+        indices = list(map(int, re.findall(r'\d+', key)))
+        indices_value_list.append((indices, float(value)))
+
+    # Sort the indices_value_list based on indices
+    indices_value_list.sort(key=lambda x: x[0])
+
+    # Extract the values into a flat list
+    values_list = [value for indices, value in indices_value_list]
+
+    # Descale the values
+    values_list = [value / 2**10 for value in values_list]
+
+    dimensions = [10]
+
+    real_output = reshape_array(values_list, dimensions)
+
     with open('ml_tests/Dense/dense_input.json', 'r') as f:
         data = json.load(f)
 
     input_data = data['in_unchanged']
     weights = data['weights_unchanged']
     bias = data['bias_unchanged']
-    real_output = data['out']
 
-    for i in range(len(real_output)):
-        print(float(real_output[i]))
-        real_output[i] = float(real_output[i])
-        real_output[i] /= 2**10
-        
+    # Compute the correct output
     correct_output = DenseInt(20, 10, 10, input_data, weights, bias)
 
-    print(f"Correct output: {correct_output}")
-    print(f"Real output: {real_output}")
+    print(f"Correct output: {correct_output}\n")
+    print(f"Real output: {real_output}\n")
+
+    correct_output_np = np.array(correct_output, dtype=float)
+    real_output_np = np.array(real_output, dtype=float)
+
+    difference = np.abs(correct_output_np - real_output_np)
+
+    print(f"Difference between outputs: \n{difference}")
 
 if __name__ == "__main__":
     main()
